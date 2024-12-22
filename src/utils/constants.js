7657ca9fbf7ad53fbaa926264bfb2db73007bfd6
Lekha <lekhasoni001@gmail.com>
2024-12-22 22:00:51 +0530

    corrected cors issue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,5 +3,7 @@ export const LOGO_URL = "https://gearfiles.gsmcdn.com/wp-content/uploads/sites/1
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65420&lng=77.23730&restaurantId=";
 export const MENU_ITEM_TYPE_KEY = "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory";
 export const RESTAURANTS_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
-export const PROXY_CORS = "https://corsproxy.org/?";
-export const generateProxyUrl = (URL) => PROXY_CORS + encodeURIComponent(URL);
+// export const PROXY_CORS = "https://corsproxy.org/?";
+export const PROXY_CORS = "https://thingproxy.freeboard.io/fetch/";
+// export const generateProxyUrl = (URL) => PROXY_CORS + encodeURIComponent(URL);
+export const generateProxyUrl = (URL) => PROXY_CORS + URL;
