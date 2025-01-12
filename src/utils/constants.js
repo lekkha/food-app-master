86390b96b936f281e0a640fe721e36e0083276f4
Lekha <lekhasoni001@gmail.com>
2025-01-12 12:44:39 +0530

    changed api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,8 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 export const LOGO_URL = "https://gearfiles.gsmcdn.com/wp-content/uploads/sites/15/2023/03/26142052/Food-Order-Pro-Logo-Green-Back-Circle.png";
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65420&lng=77.23730&restaurantId=";
 export const MENU_ITEM_TYPE_KEY = "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory";
-export const RESTAURANTS_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+// export const RESTAURANTS_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const RESTAURANTS_API = "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 // export const PROXY_CORS = "https://corsproxy.org/?";
 export const PROXY_CORS = "https://thingproxy.freeboard.io/fetch/";
 // export const generateProxyUrl = (URL) => PROXY_CORS + encodeURIComponent(URL);
